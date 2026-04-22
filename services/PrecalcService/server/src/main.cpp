f9eea15e98a040f670bbb875fa22f23f609fe3e3
yuanhao <yuanhao73@huawei.com>
2026-04-22 20:24:25 +0800

    update precalcService and RankService

--- services/PrecalcService/server/src/main.cpp
+++ services/PrecalcService/server/src/main.cpp
@@ -1,7 +1,16 @@
+// 1. 对应的头文件
 #include "precalc_server.h"
+
+// 2. 标准库头文件
+
+// 3. 系统库头文件
+
+// 4. 其他库头文件
 #include <gflags/gflags.h>
 #include <butil/logging.h>
 #include <brpc/server.h>
+
+// 5. 本项目内其他头文件
 
 int main(int argc, char* argv[]) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -28,7 +37,7 @@ int main(int argc, char* argv[]) {
     
     LOG(INFO) << "PrecalcService started on port " << FLAGS_server_port;
     LOG(INFO) << "Precalc result size: " << FLAGS_precalc_result_size_mb << " MB";
-    LOG(INFO) << "Response total size: " << FLAGS_response_total_size_kb << " KB (key + payload)";
+    LOG(INFO) << "user_feat_key size: " << FLAGS_user_feat_key_size_kb << " KB";
     LOG(INFO) << "TTL: " << FLAGS_ttl_seconds << " seconds";
     LOG(INFO) << "KVWorker address: " << FLAGS_kvworker_host << ":" << FLAGS_kvworker_port;
     
