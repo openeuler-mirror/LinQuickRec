574ea96bda3ebf04ba33bff93085cffc8038a6fe
yuanhao <yuanhao73@huawei.com>
2026-04-23 12:06:23 +0800

    修复recall服务编译错误

--- services/recall/server/src/recall_server.cpp
+++ services/recall/server/src/recall_server.cpp
@@ -209,7 +209,7 @@ void RecallServiceImpl::Recall(google::protobuf::RpcController* controller,
 }
 
 RecallServiceImpl::RecallResult process_recall_request(const RecallRequest* request) {
-    RecallResult result;
+    RecallServiceImpl::RecallResult result;
 
     std::string request_json = proto_to_json(request);
     LOG(INFO) << "Converted request to JSON: " << request_json;
