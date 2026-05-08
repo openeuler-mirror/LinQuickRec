42a8b3a637a9efc1b33f9b675de4080e5e3927b7
Haoyu <haoyu.wang141@outlook.com>
2026-05-08 11:52:48 +0800

    style: pad log level strings to 5 chars for alignment

--- common/src/logger/config.cpp
+++ common/src/logger/config.cpp
@@ -9,8 +9,8 @@ const char* LogLevelToString(LogLevel level) {
     switch (level) {
         case LogLevel::TRACE: return "TRACE";
         case LogLevel::DEBUG: return "DEBUG";
-        case LogLevel::INFO:  return "INFO";
-        case LogLevel::WARNING:  return "WARN";
+        case LogLevel::INFO:  return "INFO ";
+        case LogLevel::WARNING:  return "WARN ";
         case LogLevel::ERROR: return "ERROR";
         case LogLevel::FATAL: return "FATAL";
         default: return "UNKNOWN";
