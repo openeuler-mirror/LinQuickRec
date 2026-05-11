7c6d4058040600f49bffc6001b1a126f89cb1178
Haoyu <haoyu.wang141@outlook.com>
2026-05-11 12:58:29 +0800

    fix: downgrade heartbeat OK log to LOG_DEBUG to reduce noise

--- services/discovery/client/src/main.cpp
+++ services/discovery/client/src/main.cpp
@@ -220,7 +220,7 @@ int main(int argc, char* argv[]) {
                     LOG_WARN << "Server lost state, re-registering";
                     g_registered = false;
                 } else {
-                    LOG_INFO << "Heartbeat OK";
+                    LOG_DEBUG << "Heartbeat OK";
                 }
             }
         } else {
